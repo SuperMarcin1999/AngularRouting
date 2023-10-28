683ed41d4e7096e08dabdf84cdecbb1ea9d76258
Juliusz Ceasar <marcin@o2.pl>
2023-10-28 22:01:17 +0200

    Nowy komponent ala not found, wyswietlenie go gdy sciezka nie istnieje

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {FirstComponent} from "./first/first.component";
 import {SecondComponent} from "./second/second.component";
+import {NotFoundComponent} from "./not-found/not-found.component";
 
+// Wykonuja sie w kolejnosci (od gory do dolu)
 const routes: Routes = [
   {path: 'first', component: FirstComponent},
   {path: 'second', component: SecondComponent},
+  {path: '**', component: NotFoundComponent}
 ];
 
 @NgModule({
